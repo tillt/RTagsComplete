d0a7bb13ba84277672fcbaa24e5b6fd6bab8d601
Till Toenshoff <toenshoff@me.com>
2018-07-29 15:16:37 +0200

    Fixed testing module omissions.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,2 @@
 """Testing module."""
-__all__ = ("test_settings", "test_vc", "test_progress", "test_fixits")
+__all__ = ("test_settings", "test_jobs", "test_idle", "test_vc", "test_progress", "test_fixits")
