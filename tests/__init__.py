e09d2238b1966bcf61647be57d62bec0e66ae770
Till Toenshoff <toenshoff@me.com>
2018-11-02 17:09:40 -0700

    Fixed style issue in tests/__init__.py.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,8 @@
 """Testing module."""
-__all__ = ("test_settings", "test_jobs", "test_idle", "test_vc", "test_progress", "test_fixits")
+__all__ = (
+    "test_settings",
+    "test_jobs",
+    "test_idle",
+    "test_vc",
+    "test_progress",
+    "test_fixits")
