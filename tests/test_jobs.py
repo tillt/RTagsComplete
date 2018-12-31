81d9d6b9370bcee44e46a2f38da3db5852b59590
Till Toenshoff <toenshoff@me.com>
2018-12-31 01:58:41 +0100

    Nothing vastly imoprtant.

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -42,6 +42,13 @@ class TestJob(jobs.RTagsJob):
 class TestJobController(TestCase):
     """Test Job Controller."""
 
+    def setUp(self):
+        self.set_up()
+
+    def tearDown(self):
+        jobs.JobController.stop_all()
+        self.tear_down()
+
     def command_done(self, future, **kwargs):
         log.debug("Command done callback hit {}".format(future))
 
